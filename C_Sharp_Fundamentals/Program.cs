b8723120bf86b3e398514018d0f7cfb4d15211b5
The Sharps <Sharp@Thes-MacBook-Air.local>
2018-04-16 12:31:14 -0400

    initial push

--- C_Sharp_Fundamentals/Program.cs
+++ C_Sharp_Fundamentals/Program.cs
@@ -6,7 +6,22 @@ namespace C_Sharp_Fundamentals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name);
+            if(hoursOfSleep > 8)
+            {
+                Console.WriteLine("You are well rested");
+            }
+            else 
+            {
+                Console.WriteLine("You need more sleep.");
+            }
+
         }
     }
 }
